b162d6d1137c725818257cbcd0da53d300cedd54
Monty A <ma@surgicalcoder.com>
2016-08-28 18:23:49 +0100

    Same issue as MVC with dots.

--- Audit.WebApi/AuditApiAttribute.cs
+++ Audit.WebApi/AuditApiAttribute.cs
@@ -107,7 +107,7 @@ namespace Audit.WebApi
             IDictionary<string, string> dict = new Dictionary<string, string>();
             foreach (var k in col)
             {
-                dict.Add(k.Key, string.Join(", ", k.Value));
+                dict.Add(k.Key.Replace(".","_"), string.Join(", ", k.Value));
             }
             return dict;
         }
