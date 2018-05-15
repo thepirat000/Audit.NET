7b381c7ef3f43c7b54f4e852246aa064ceaf7cde
Federico Colombo <thepirat000@hotmail.com>
2018-05-15 16:25:40 -0500

    Adding filedataprovider test

--- test/Audit.UnitTest/FileDataProviderTests.cs
+++ test/Audit.UnitTest/FileDataProviderTests.cs
@@ -1,4 +1,5 @@
-﻿using Audit.Core;
+﻿using Newtonsoft.Json;
+using Audit.Core;
 using Audit.Core.Providers;
 using Newtonsoft.Json.Linq;
 using NUnit.Framework;
@@ -24,6 +25,7 @@ namespace Audit.UnitTest
         [SetUp]
         public void Setup()
         {
+            Audit.Core.Configuration.DataProvider = new FileDataProvider();
             Audit.Core.Configuration.AuditDisabled = false;
             Audit.Core.Configuration.ResetCustomActions();
             _directory = Path.Combine(Path.GetTempPath(), "events");
@@ -89,6 +91,64 @@ namespace Audit.UnitTest
             Assert.AreEqual(2, ev.CustomFields.Count);
             Assert.AreEqual(1, ev.CustomFields["Id"]);
             Assert.AreEqual(JObject.Parse("{\"Id\": 1}").ToString(), ev.CustomFields["Inner"].ToString());
+        }
+
+        [Test]
+        public void Test_FileDataProvider_Error()
+        {
+            var loop = new Loop() { Id = 1 };
+            loop.Inner = loop;
+
+            var fdp = new FileDataProvider()
+            {
+                DirectoryPath = _directory,
+                FilenameBuilder = x => x.EventType
+            };
+
+            Configuration.Setup().UseFileLogProvider(_ => _
+                .Directory(_directory)
+                .FilenameBuilder(x => x.EventType)
+                .JsonSettings(new JsonSerializerSettings() { ReferenceLoopHandling = ReferenceLoopHandling.Error }));
+
+            var guid = "x" + Guid.NewGuid().ToString();
+            try
+            {
+                AuditScope.CreateAndSave(guid, loop);
+                Assert.Fail("Should not get here. JsonSettings not respected?");
+            }
+            catch (JsonSerializationException ex)
+            {
+                Assert.IsTrue(ex.Message.ToLower().Contains("loop detected"));
+            }
+        }
+
+        [Test]
+        public async Task Test_FileDataProvider_ErrorAsync()
+        {
+            var loop = new Loop() { Id = 1 };
+            loop.Inner = loop;
+
+            var fdp = new FileDataProvider()
+            {
+                DirectoryPath = _directory,
+                FilenameBuilder = x => x.EventType
+            };
+
+            Configuration.Setup().UseFileLogProvider(_ => _
+                .Directory(_directory)
+                .FilenameBuilder(x => x.EventType)
+                .JsonSettings(new JsonSerializerSettings() { ReferenceLoopHandling = ReferenceLoopHandling.Error }));
+
+            var guid = "x" + Guid.NewGuid().ToString();
+            try
+            {
+                await AuditScope.CreateAndSaveAsync(guid, loop);
+                Assert.Fail("Should not get here. JsonSettings not respected?");
+            }
+            catch (JsonSerializationException ex)
+            {
+                Assert.IsTrue(ex.Message.ToLower().Contains("loop detected"));
+            }
         }
 
     }
