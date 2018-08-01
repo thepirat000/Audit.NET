803648b1d631523ec1464f27633d342f0a837ee2
Federico Colombo <thepirat000@hotmail.com>
2018-08-01 15:42:19 -0500

    fix mvc template

--- templates/MVC/content/Audit.Mvc.Template/AuditStartup.cs
+++ templates/MVC/content/Audit.Mvc.Template/AuditStartup.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.DependencyInjection;
+using Audit.Mvc;
 
 namespace Audit.Mvc.Template
 {
