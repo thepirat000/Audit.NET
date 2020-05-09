90c7bb03d58a28adc2465d047121eb0b844dac82
thepirat000 <thepirat000@hotmail.com>
2020-05-09 13:02:56 -0500

    update push.cmd

--- src/push.cmd
+++ src/push.cmd
@@ -29,30 +29,30 @@ del "Audit.NET.DynamoDB\bin\debug\*.symbols.nupkg"
 del "Audit.HttpClient\bin\debug\*.symbols.nupkg"
 del "Audit.NET.NLog\bin\debug\*.symbols.nupkg"
 
-nuget push "audit.net\bin\debug\*.nupkg" -source %1
-nuget push "audit.mvc\bin\debug\*.nupkg" -source %1
-nuget push "Audit.Mvc.Core\bin\debug\*.nupkg" -source %1
-nuget push "audit.webapi\bin\debug\*.nupkg" -source %1
-nuget push "Audit.WebApi.Core\bin\debug\*.nupkg" -source %1
-nuget push "audit.net.azuredocumentdb\bin\debug\*.nupkg" -source %1
-nuget push "audit.net.mongodb\bin\debug\*.nupkg" -source %1
-nuget push "audit.net.sqlserver\bin\debug\*.nupkg" -source %1
-nuget push "audit.net.mysql\bin\debug\*.nupkg" -source %1
-nuget push "audit.EntityFramework\bin\debug\*.nupkg" -source %1
-nuget push "audit.EntityFramework.Core\bin\debug\*.nupkg" -source %1
-nuget push "audit.EntityFramework.Identity\bin\debug\*.nupkg" -source %1
-nuget push "audit.EntityFramework.Identity.Core\bin\debug\*.nupkg" -source %1
-nuget push "audit.Wcf\bin\debug\*.nupkg" -source %1
-nuget push "Audit.NET.AzureStorage\bin\debug\*.nupkg" -source %1
-nuget push "Audit.DynamicProxy\bin\debug\*.nupkg" -source %1
-nuget push "Audit.NET.Udp\bin\debug\*.nupkg" -source %1
-nuget push "Audit.NET.Redis\bin\debug\*.nupkg" -source %1
-nuget push "Audit.NET.PostgreSql\bin\debug\*.nupkg" -source %1
-nuget push "Audit.FileSystem\bin\debug\*.nupkg" -source %1
-nuget push "Audit.SignalR\bin\debug\*.nupkg" -source %1
-nuget push "Audit.NET.log4net\bin\debug\*.nupkg" -source %1
-nuget push "Audit.NET.Elasticsearch\bin\debug\*.nupkg" -source %1
-nuget push "Audit.NET.EventLog.Core\bin\debug\*.nupkg" -source %1
-nuget push "Audit.NET.DynamoDB\bin\debug\*.nupkg" -source %1
-nuget push "Audit.HttpClient\bin\debug\*.nupkg" -source %1
-nuget push "Audit.NET.NLog\bin\debug\*.nupkg" -source %1
+nuget push "audit.net\bin\debug\*.nupkg" -NoSymbols -source %1
+nuget push "audit.mvc\bin\debug\*.nupkg" -NoSymbols -source %1
+nuget push "Audit.Mvc.Core\bin\debug\*.nupkg" -NoSymbols -source %1
+nuget push "audit.webapi\bin\debug\*.nupkg" -NoSymbols -source %1
+nuget push "Audit.WebApi.Core\bin\debug\*.nupkg" -NoSymbols -source %1
+nuget push "audit.net.azuredocumentdb\bin\debug\*.nupkg" -NoSymbols -source %1
+nuget push "audit.net.mongodb\bin\debug\*.nupkg" -NoSymbols -source %1
+nuget push "audit.net.sqlserver\bin\debug\*.nupkg" -NoSymbols -source %1
+nuget push "audit.net.mysql\bin\debug\*.nupkg" -NoSymbols -source %1
+nuget push "audit.EntityFramework\bin\debug\*.nupkg" -NoSymbols -source %1
+nuget push "audit.EntityFramework.Core\bin\debug\*.nupkg" -NoSymbols -source %1
+nuget push "audit.EntityFramework.Identity\bin\debug\*.nupkg" -NoSymbols -source %1
+nuget push "audit.EntityFramework.Identity.Core\bin\debug\*.nupkg" -NoSymbols -source %1
+nuget push "audit.Wcf\bin\debug\*.nupkg" -NoSymbols -source %1
+nuget push "Audit.NET.AzureStorage\bin\debug\*.nupkg" -NoSymbols -source %1
+nuget push "Audit.DynamicProxy\bin\debug\*.nupkg" -NoSymbols -source %1
+nuget push "Audit.NET.Udp\bin\debug\*.nupkg" -NoSymbols -source %1
+nuget push "Audit.NET.Redis\bin\debug\*.nupkg" -NoSymbols -source %1
+nuget push "Audit.NET.PostgreSql\bin\debug\*.nupkg" -NoSymbols -source %1
+nuget push "Audit.FileSystem\bin\debug\*.nupkg" -NoSymbols -source %1
+nuget push "Audit.SignalR\bin\debug\*.nupkg" -NoSymbols -source %1
+nuget push "Audit.NET.log4net\bin\debug\*.nupkg" -NoSymbols -source %1
+nuget push "Audit.NET.Elasticsearch\bin\debug\*.nupkg" -NoSymbols -source %1
+nuget push "Audit.NET.EventLog.Core\bin\debug\*.nupkg" -NoSymbols -source %1
+nuget push "Audit.NET.DynamoDB\bin\debug\*.nupkg" -NoSymbols -source %1
+nuget push "Audit.HttpClient\bin\debug\*.nupkg" -NoSymbols -source %1
+nuget push "Audit.NET.NLog\bin\debug\*.nupkg" -NoSymbols -source %1
