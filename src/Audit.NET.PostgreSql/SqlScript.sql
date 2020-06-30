93863f9c6935e083a9881b4ff46c0413336c2bb1
Federico Daniel Colombo <thepirat000@hotmail.com>
2020-06-30 13:44:49 -0500

    Update SqlScript.sql

--- src/Audit.NET.PostgreSql/SqlScript.sql
+++ src/Audit.NET.PostgreSql/SqlScript.sql
@@ -4,7 +4,7 @@
     inserted_date timestamp without time zone NOT NULL DEFAULT now(),
     updated_date timestamp without time zone NOT NULL DEFAULT now(),
     data jsonb NOT NULL,
-	event_type varchar(50) NULL,
+    event_type varchar(50) NULL,
     "user" varchar(50) NULL,
     CONSTRAINT event_pkey PRIMARY KEY (id)
 )
