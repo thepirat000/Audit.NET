edb091db8add8316d702789526a28f784a58d912
Albin Forsberg <albin.forsberg@albia.se>
2023-06-14 10:08:03 +0200

    Changed target version to net7

--- test/Audit.EntityFramework.UnitTest/Model.Designer.cs
+++ test/Audit.EntityFramework.UnitTest/Model.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'D:\MIOS\Proyectos .NET\Audit.NET\test\Audit.EntityFramework.UnitTest\Model.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\albinf\m2m\git\Audit.NET\test\Audit.EntityFramework.UnitTest\Model.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
