69a39d44187ed9051ca763a4afc24435b8581903
Marco van den Oever <marco.vandenoever@datarotonde.com>
2025-04-03 11:35:45 +0200

    Workspace

--- src/Audit.NET.OpenSearch/Extensions/OpenSearchExtensions.cs
+++ src/Audit.NET.OpenSearch/Extensions/OpenSearchExtensions.cs
@@ -5,16 +5,16 @@ namespace Audit.OpenSearch.Extensions;
 
 public static class OpenSearchExtensions
 {
-	public static bool TryGetOriginalException<T>(this T resp, out Exception? exception)
-		where T : ResponseBase
-	{
-		if (resp.ApiCall?.OriginalException is not null)
-		{
-			exception = resp.ApiCall.OriginalException;
-			return true;
-		}
+    public static bool TryGetOriginalException<T>(this T resp, out Exception? exception)
+        where T : ResponseBase
+    {
+        if (resp.ApiCall?.OriginalException is not null)
+        {
+            exception = resp.ApiCall.OriginalException;
+            return true;
+        }
 
-		exception = null;
-		return false;
-	}
+        exception = null;
+        return false;
+    }
 }
