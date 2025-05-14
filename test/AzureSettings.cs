d248bfe467d109f0be0bde26e176fa330d4bb68e
thepirat000 <thepirat000@hotmail.com>
2025-05-13 21:18:28 -0600

    azure events hub emulator

--- test/AzureSettings.cs
+++ test/AzureSettings.cs
@@ -4,13 +4,13 @@ namespace Audit.IntegrationTest
 {
     public static class AzureSettings
     {
-        public static string AzureBlobAccountName => GetFromEnv("AZUREBLOBACCOUNTNAME");
-        public static string AzureBlobAccountKey => GetFromEnv("AZUREBLOBACCOUNTKEY");
-        public static string AzureBlobCnnString => GetFromEnv("AZUREBLOBCNNSTRING");
-        public static string AzureTableCnnString => GetFromEnv("AZURETABLECNNSTRING");
-        public static string AzureDocDbUrl => GetFromEnv("AZUREDOCDBURL");
-        public static string AzureDocDbAuthKey => GetFromEnv("AZUREDOCDBAUTHKEY");
-        public static string AzureEventHubCnnString => GetFromEnv("AZUREEVTHUBCNNSTRING");
+        public static string AzureBlobAccountName => GetFromEnv("AUDIT_NET_AZUREBLOBACCOUNTNAME");
+        public static string AzureBlobAccountKey => GetFromEnv("AUDIT_NET_AZUREBLOBACCOUNTKEY");
+        public static string AzureBlobCnnString => GetFromEnv("AUDIT_NET_AZUREBLOBCNNSTRING");
+        public static string AzureTableCnnString => GetFromEnv("AUDIT_NET_AZURETABLECNNSTRING");
+        public static string AzureDocDbUrl => GetFromEnv("AUDIT_NET_AZUREDOCDBURL");
+        public static string AzureDocDbAuthKey => GetFromEnv("AUDIT_NET_AZUREDOCDBAUTHKEY");
+        public static string AzureEventHubCnnString => GetFromEnv("AUDIT_NET_AZUREEVTHUBCNNSTRING");
 
         public static string ElasticSearchUrl => "http://elastic:elastic@127.0.0.1:9200";
         public static string OpenSearchUrl => "http://admin:Messi1708!!!@127.0.0.1:9250";
@@ -18,7 +18,7 @@ namespace Audit.IntegrationTest
 
         private static string GetFromEnv(string key, string @default = null)
         {
-            var varName = "AUDIT_NET_" + key.ToUpper();
+            var varName = key.ToUpper();
             var value = Environment.GetEnvironmentVariable(varName) ?? @default ?? throw new Exception($"No environment variable or default set for variable '{key}'");
             return value;
         }
